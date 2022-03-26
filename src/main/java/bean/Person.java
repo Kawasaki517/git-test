d75472ee000518c0644cdcc7c971344790dd37e1
gennsy <gennsy@qq.com>
2022-03-26 12:37:23 +0800

    second commit

---
+++ src/main/java/bean/Person.java
@@ -0,0 +1,38 @@
+package bean;
+
+import org.omg.PortableInterceptor.Interceptor;
+
+public class Person {
+    String name;
+    Integer id;
+    public Person(){}
+
+    public Person(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
